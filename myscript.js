5e66790cb3fd446cb9f191cc4848ab87ef1201ce
FrancescoPierfederici <francescopierf@gmail.com>
2021-04-07 17:04:23 +0200

    Aggiunge Javascript esercizio

---
+++ myscript.js
@@ -0,0 +1,9 @@
+var namess = prompt(" Inserisci il tuo Nome");
+
+var lastName = prompt(" Inserisci il tuo Cognome");
+
+var color = prompt("Inserisci il tuo Colore preferito");
+
+// console.log(namess + lastName + color + "21");
+
+document.writeln(namess + lastName + color + "21");
